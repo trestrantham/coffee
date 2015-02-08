cb9bf41a15435ab170f99d707f13826fba2cad59
Tres Trantham <tres@trestrantham.com>
2015-02-08 14:16:21 -0500

    Add deps for test; Add blacksmith

--- mix.exs
+++ mix.exs
@@ -2,27 +2,55 @@ defmodule Coffee.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :coffee,
-     version: "0.0.1",
-     elixir: "~> 1.0",
-     elixirc_paths: ["lib", "web"],
-     compilers: [:phoenix] ++ Mix.compilers,
-     deps: deps]
+    [
+      app: :coffee,
+      version: "0.0.1",
+      elixir: "~> 1.0",
+      elixirc_paths: ["lib", "web"],
+      compilers: [:phoenix] ++ Mix.compilers,
+      deps: deps(Mix.env)
+    ]
   end
 
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
   def application do
-    [mod: {Coffee, []},
-     applications: [:phoenix, :cowboy, :logger]]
+    [
+      mod: {Coffee, []},
+      applications: applications(Mix.env)
+    ]
+  end
+
+  defp applications(:prod) do
+    [:phoenix, :cowboy, :logger]
+  end
+
+  defp applications(:test) do
+    applications(:all) ++ [:blacksmith]
+  end
+
+  defp applications(_) do
+    applications(:prod)
   end
 
   # Specifies your project dependencies
   #
   # Type `mix help deps` for examples and options
-  defp deps do
-    [{:phoenix, "~> 0.8.0"},
-     {:cowboy, "~> 1.0"}]
+  defp deps(:prod) do
+    [
+      {:phoenix, "~> 0.8.0"},
+      {:cowboy, "~> 1.0"}
+    ]
+  end
+
+  defp deps(:test) do
+    deps(:prod) ++ [
+      {:blacksmith, git: "git://github.com/batate/blacksmith.git"}
+    ]
+  end
+
+  defp deps(_) do
+    deps(:prod)
   end
 end
