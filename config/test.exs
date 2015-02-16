d673844f90e8378035100a02866b2a571cef4446
Tres Trantham <tres@trestrantham.com>
2015-02-16 00:06:39 -0500

    Ensure endpoint is started with tests

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,8 @@
 use Mix.Config
 
 config :coffee, Coffee.Endpoint,
-  http: [port: System.get_env("PORT") || 4001]
+  http: [port: System.get_env("PORT") || 4001],
+  server: true
 
 # Print only warnings and errors during test
 config :logger, level: :warn
